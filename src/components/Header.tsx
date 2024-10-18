afb69782247a81dcaa2bf59426209ac8d9303244
HyeonjuLee <leehj595976@gmail.com>
2024-10-18 22:21:05 +0900

    Add Tailwind CSS setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <div className="text-3xl">Movie</div>
+          <div>{/* <input /> */}</div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
