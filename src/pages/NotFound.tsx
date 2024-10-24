75a2108e1530aaa589dc53bb78f6e9a019a47d27
HyeonjuLee <leehj595976@gmail.com>
2024-10-25 00:31:59 +0900

    Not found 페이지 구현

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,24 @@
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const NotFound = () => {
-  return <div>잘못된 경로입니다.</div>;
+  return (
+    <main className="mt-[30px]">
+      <section>
+        <div className="w-[1120px] min-h-[383px] mx-auto mb-[80px]">
+          <div className="flex flex-col items-center">
+            <div style={{ marginBottom: 30 }}>
+              <FontAwesomeIcon
+                icon={faTriangleExclamation}
+                className="text-white w-[32px] h-[32px]"
+              />
+            </div>
+            <span className="text-white text-[20px]">잘못된 경로입니다.</span>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default NotFound;
