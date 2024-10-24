9cb05a8f66df1826cd101ece9f0d5dbcebc7d462
HyeonjuLee <leehj595976@gmail.com>
2024-10-24 20:02:37 +0900

    디테일 상단 큰 이미지 컴포넌트 화

---
+++ src/components/VideoBigImage.tsx
@@ -0,0 +1,75 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay, faStar } from "@fortawesome/free-solid-svg-icons";
+import moment from "moment";
+import { MovieInfoProps } from "../types";
+import { GET_BACKPOSTER_URI } from "../utils/constants";
+
+interface VideoBigImageProps {
+  loading: boolean;
+  movieInfo?: MovieInfoProps;
+  onOpenModal?: () => void;
+}
+
+const VideoBigImage = ({
+  loading,
+  movieInfo,
+  onOpenModal,
+}: VideoBigImageProps) => {
+  return (
+    <section className="relative h-[590px]">
+      {!loading && movieInfo ? (
+        <div
+          className="relative w-[1120px] h-[590px] mx-auto bg-cover bg-center rounded-2xl flex items-center justify-center"
+          style={{
+            backgroundImage: `linear-gradient(to bottom left,rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 1)), url(${GET_BACKPOSTER_URI}${movieInfo?.backdrop_path})`,
+          }}
+        >
+          <button
+            className="align-middle cursor-pointer bg-transparent border-0"
+            onClick={onOpenModal}
+          >
+            <FontAwesomeIcon
+              icon={faCirclePlay}
+              className="flex text-white text-[50px] justify-center items-center"
+            />
+          </button>
+
+          <div className="absolute flex flex-col z-[40px] left-[20px] bottom-[40px] ml-[20px]">
+            <div className="flex items-center mb-[24px]">
+              <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">
+                {moment(movieInfo?.release_date).format("YYYY.MM.DD")}
+              </span>
+
+              <div className="inline-block w-[3px] h-[3px] mt-[12px] mx-[8px] rounded-full bg-[#a5a5a5] align-middle"></div>
+              <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">{`${movieInfo?.runtime}분`}</span>
+
+              <div className="inline-block w-[3px] h-[3px] mt-[12px] mx-[8px] rounded-full bg-[#a5a5a5] align-middle"></div>
+              {movieInfo?.genres && (
+                <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">
+                  {movieInfo?.genres.map((g) => g.name).join(", ")}
+                </span>
+              )}
+
+              <FontAwesomeIcon
+                icon={faStar}
+                className="flex items-center mx-2 text-red-600"
+              />
+
+              <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">
+                {`평점 : ${(movieInfo?.vote_average ?? 0).toFixed(1)}`}
+              </span>
+            </div>
+
+            <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">
+              {movieInfo?.tagline}
+            </span>
+          </div>
+        </div>
+      ) : (
+        <div className="relative w-[1120px] h-[590px] mx-auto bg-cover bg-center rounded-2xl flex items-center justify-center bg-gray-200 overflow-hidden animate-pulse"></div>
+      )}
+    </section>
+  );
+};
+
+export default VideoBigImage;
--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -1,17 +1,13 @@
 import { useParams } from "react-router-dom";
 import useMovieStore from "../stores/movieList";
 import { useEffect, useMemo, useState } from "react";
-import { GET_BACKPOSTER_URI } from "../utils/constants";
-import { faCirclePlay, faStar } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import TrailerModal from "../components/TrailerModal";
-import moment from "moment";
+import VideoBigImage from "../components/VideoBigImage";
 
 const MovieDetail = () => {
   const { id } = useParams<{ id: string }>();
   const {
     isDetailLoading,
-    isTrailerLoading,
     movieInfo,
     movieTrailerInfo,
     getMovieDetail,
@@ -32,63 +28,14 @@ const MovieDetail = () => {
 
   console.log("id", id);
   console.log("movieInfo screen", movieInfo);
+
   return (
-    <section>
-      {!isDetailLoading && (
-        <div className="relative h-[590px]">
-          <div
-            className="relative w-[1120px] h-[590px] mx-auto bg-cover bg-center rounded-2xl flex items-center justify-center"
-            style={{
-              backgroundImage: `linear-gradient(to bottom left,rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 1)), url(${GET_BACKPOSTER_URI}${movieInfo?.backdrop_path})`,
-            }}
-          >
-            <button
-              className="align-middle cursor-pointer bg-transparent border-0"
-              onClick={() => setOpenTrailerModal(true)}
-            >
-              <FontAwesomeIcon
-                icon={faCirclePlay}
-                className="flex text-white text-[50px] justify-center items-center"
-              />
-            </button>
-
-            <div className="absolute flex flex-col z-[40px] left-[20px] bottom-[40px] ml-[20px]">
-              <div className="flex mb-[24px]">
-                <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">
-                  {moment(movieInfo?.release_date).format("YYYY.MM.DD")}
-                </span>
-
-                <div className="inline-block w-[3px] h-[3px] mt-[12px] mx-[8px] rounded-full bg-[#a5a5a5] align-middle"></div>
-                <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">{`${movieInfo?.runtime}분`}</span>
-
-                <div className="inline-block w-[3px] h-[3px] mt-[12px] mx-[8px] rounded-full bg-[#a5a5a5] align-middle"></div>
-                {movieInfo?.genres && (
-                  <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">
-                    {movieInfo?.genres.map((g) => g.name).join(", ")}
-                  </span>
-                )}
-
-                <FontAwesomeIcon
-                  icon={faStar}
-                  className="flex items-center mx-2 text-red-600"
-                />
-
-                <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">
-                  {`평점 : ${(movieInfo?.vote_average ?? 0).toFixed(1)}`}
-                </span>
-              </div>
-
-              <span className="max-w-[760px] text-[16px] text-[#a5a5a5] leading-[24px] break-all">
-                {movieInfo?.tagline}
-              </span>
-            </div>
-          </div>
-        </div>
-      )}
-
-      {isDetailLoading && (
-        <div className="relative w-[1120px] h-[590px] mx-auto bg-cover bg-center rounded-2xl flex items-center justify-center bg-gray-200 overflow-hidden animate-pulse"></div>
-      )}
+    <main>
+      <VideoBigImage
+        loading={isDetailLoading}
+        movieInfo={movieInfo}
+        onOpenModal={() => setOpenTrailerModal(true)}
+      />
 
       {openTrailerModal && (
         <TrailerModal
@@ -96,7 +43,7 @@ const MovieDetail = () => {
           onClose={() => setOpenTrailerModal(false)}
         />
       )}
-    </section>
+    </main>
   );
 };
 
